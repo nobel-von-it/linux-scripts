ffc7ce6927762b412a81b25b3b867755499efc1c
nobel-von-it <maksimdavydenko12@gmail.com>
2025-03-28 22:39:38 +0700

    fix: maybe work for any unix user

--- git.sh
+++ git.sh
@@ -4,6 +4,5 @@ git clone git@github.com:nobel-von-it/rust_kata ~/Dev/Rusty/rust_kata
 git clone git@github.com:nobel-von-it/rvim ~/.config/nvim
 git clone git@github.com:nobel-von-it/rls ~/Dev/Rusty/rls
 
-
 git config --global user.email maksimdavydenko12@gmail.com
 git config --global user.name nobel-von-it
--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
-wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz -P /home/nerd/Downloads/
-sudo tar -C /usr/local/ -xzf /home/nerd/Downloads/go1.23.0.linux-amd64.tar.gz 
+wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz -P ~/Downloads/
+sudo tar -C /usr/local/ -xzf ~/Downloads/go1.23.0.linux-amd64.tar.gz 
