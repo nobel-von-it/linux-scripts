4fd0329c934ef7260437d0dd51d606c467390330
nobel-von-it <maksimdavydenko12@gmail.com>
2025-05-06 21:36:35 +0700

    fix: email change in ssh

--- rust.sh
+++ rust.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 OS=
 VER=
 
--- sshgit.sh
+++ sshgit.sh
@@ -1,4 +1,20 @@
-ssh-keygen -t ed25519 -C "maksimdavydenko12@gmail.com"
+#!/bin/bash
+
+EMAIL="maksimdavydenko12@gmail.com"
+
+echo "Do you want to change default email $EMAIL? (y/n)"
+read -r ans
+
+if [[ "$ans" = "y" ]]
+then
+    echo "Write email: "
+    read -r email
+    EMAIL=$email
+fi
+
+echo "Creating ssh key"
+
+ssh-keygen -t ed25519 -C "$EMAIL"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
