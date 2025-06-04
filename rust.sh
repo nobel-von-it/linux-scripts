060935782deafaaae7e991612f5ead80dfc84da7
nobel-von-it <maksimdavydenko12@gmail.com>
2025-06-04 11:48:51 +0700

    Improve rust installation with official script

--- rust.sh
+++ rust.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-OS=$(./getos)
+# OS=$(./getos)
+#
+# case "$OS" in
+#     *"Arch"*)
+#         if pacman -Q &>/dev/null
+#         then
+#             echo "Rustup already installed in system"
+#         else 
+#             yes | sudo pacman -Suy rustup
+#         fi
+#         ;;
+#     "Ubuntu")
+#         if dpkg -s rustup &>/dev/null
+#         then
+#             echo "Rustup already installed in system"
+#         else
+#             sudo apt update && sudo apt upgrade -y
+#             sudo apt install rustup -y
+#         fi
+#         ;;
+#     *)
+#         echo "Distro not support yet"
+#         exit 1
+#         ;;
+#
+# esac
 
-case "$OS" in
-    *"Arch"*)
-        if pacman -Q &>/dev/null
-        then
-            echo "Rustup already installed in system"
-        else 
-            yes | sudo pacman -Suy rustup
-        fi
-        ;;
-    "Ubuntu")
-        if dpkg -s rustup &>/dev/null
-        then
-            echo "Rustup already installed in system"
-        else
-            sudo apt update && sudo apt upgrade -y
-            sudo apt install rustup -y
-        fi
-        ;;
-    *)
-        echo "Distro not support yet"
-        exit 1
-        ;;
-
-esac
+# Founded better solution
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 rustup default stable
 rustup component add rust-analyzer
